f51abca27231aef043df2e7500952c9c51892ed4
Robincloud83 <163537394+Robincloud83@users.noreply.github.com>
2024-05-09 15:06:34 +0530

    Update hello.js

--- hello.js
+++ hello.js
@@ -1,1 +1,11 @@
-hello sir
+// app.js
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end('Hello, World!\n');
+});
+
+server.listen(3009, () => {
+    console.log('Server running at http://localhost:3009/');
+});
